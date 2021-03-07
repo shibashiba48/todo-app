bb28477a5ad67418b129aa11e3429187a38f6ba3
shibashiba48 <t1257476@gmail.com>
2021-03-07 22:19:56 +0900

    create todo-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { todoList } from "./todo-list";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello Angular!!!';
+  todoList = todoList;
 }
